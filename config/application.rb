e0ffe9d736cefb9c37d55dce5a2a7b589729a421
Artem Onoprienko <tema_on@ukr.net>
2017-12-25 12:48:04 +0200

    added auto_annotate_model task

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module PresalePlatform
     config.load_defaults 5.1
 
     Rails.application.config.autoload_paths << Rails.root.join('app/services', '*.rb')
+    Rails.application.config.autoload_paths += Dir[File.join(Rails.root, 'lib', '*.rb')].each {|l| require l }
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
